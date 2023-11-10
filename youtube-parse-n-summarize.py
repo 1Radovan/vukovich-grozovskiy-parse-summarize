ab505d63020821ca1e843d3ad46a89a9a5a7de26
markparserov <141366655+markparserov@users.noreply.github.com>
2023-11-10 17:17:51 +0300

    Update youtube-parse-n-summarize.py

--- youtube-parse-n-summarize.py
+++ youtube-parse-n-summarize.py
@@ -5,27 +5,31 @@ from transformers import TFBartForConditionalGeneration, BartTokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def get_subtitles(video_url):
+    """Parses subtitles"""
     try:
         video_id = video_url.split('v=')[1]
         transcripts = YouTubeTranscriptApi.get_transcript(video_id, languages=['ru', 'en'])
         text = ' '.join(transcript['text'] for transcript in transcripts)
         return text
     except Exception as e:
-        print(f"Ошибка при получении субтитров: {e}")
+        print(f"Error: {e}")
         return None
 
 def save_text_to_file(text, output_file):
+    """Saves the result to a file"""
     try:
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write(text)
-        print(f"Текст субтитров сохранен в файл: {output_file}")
+        print(f"Subtitles text successfully loaded to a file: {output_file}")
     except Exception as e:
-        print(f"Ошибка при сохранении файла: {e}")
+        print(f"Error: {e}")
 
 def split_text_into_chunks(text, chunk_size=10000):
+    """Splits text into chunks"""
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
 def translate_and_add_periods(video_url, output_file):
+    """Translates the subtitles to English and adds dots to the text"""
     try:
         video_id = video_url.split('v=')[1]
         transcripts = YouTubeTranscriptApi.get_transcript(video_id, languages=['ru', 'en'])
@@ -36,7 +40,7 @@ def translate_and_add_periods(video_url, output_file):
         target_language = 'en'
         chunk_size = 10000
 
-        # Split the text into chunks of 10000 characters each
+        # Splitting the text into chunks of 10000 characters each
         text_chunks = split_text_into_chunks(subtitles_text, chunk_size)
 
         translated_texts = []
@@ -60,11 +64,11 @@ def translate_and_add_periods(video_url, output_file):
                 translated_text = response.json()['translations'][0]['text']
                 translated_texts.append(translated_text)
             else:
-                print(f"Ошибка при переводе текста: {response.text}")
+                print(f"Translation error: {response.text}")
 
         translated_subtitles = ' '.join(translated_texts)
 
-        # Вызываем вторую функцию для добавления точек по смыслу
+        # Adding dots
         try:
             nlp = spacy.load("en_core_web_sm")
             doc = nlp(translated_subtitles)
@@ -76,16 +80,17 @@ def translate_and_add_periods(video_url, output_file):
 
             with open(output_file, 'w', encoding='utf-8') as file:
                 file.write(text_with_periods)
-            print(f"Текст субтитров на английском языке с точками сохранен в файл: {output_file}")
+            print(f"Subtitles with dots saved to a file: {output_file}")
 
         except Exception as e:
-            print(f"Ошибка при обработке файла: {e}")
+            print(f"Processing error: {e}")
 
     except Exception as e:
-        print(f"Ошибка при получении субтитров: {e}")
+        print(f"Unable to get the subtitles: {e}")
 
 def summarize_text(initial_text, max_length=512, min_length=256, num_beams=10):
-    # Check if CUDA GPU is available and enable memory growth
+    """Summarizes the text"""
+    # Checking if CUDA GPU is available and enable memory growth
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
         try:
@@ -93,15 +98,15 @@ def summarize_text(initial_text, max_length=512, min_length=256, num_beams=10):
         except RuntimeError as e:
             print(e)
 
-    # Load pre-trained BART model and tokenizer
+    # Loading pre-trained BART model and tokenizer
     model_name = "facebook/bart-large-cnn"
     tokenizer = BartTokenizer.from_pretrained(model_name)
     model = TFBartForConditionalGeneration.from_pretrained(model_name)
 
-    # Tokenize the input text
+    # Tokenizing the input text
     inputs = tokenizer.encode("summarize: " + initial_text, return_tensors="tf", max_length=1024, truncation=True)
 
-    # Generate the summary
+    # Generating the summary
     summary_ids = model.generate(
         inputs,
         max_length=max_length,
@@ -111,12 +116,12 @@ def summarize_text(initial_text, max_length=512, min_length=256, num_beams=10):
         num_return_sequences=10
     )
 
-    # Decode the summary tokens back to text
+    # Decoding the summary tokens back to text
     summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary_text
 
 if __name__ == "__main__":
-    video_url = input("Введите ссылку на YouTube-видео: ")
+    video_url = input("Enter a YouTube video link: ")
     output_file = 'output.txt'
 
     translate_and_add_periods(video_url, output_file)
@@ -129,4 +134,4 @@ if __name__ == "__main__":
     with open("summarized_output.txt", 'w', encoding='utf-8') as output:
         output.write(summarized_text)
 
-    print(f"Итоговый текст с субтитрами и саммари сохранен в файл: summarized_output.txt")
+    print(f"Result with subtitles and summary saved to file: summarized_output.txt")
